27aa8045b8a51cadfe6ccb1ce3bb59bcfed3956c
Taliesin <tdcochran2@gmail.com>
2017-11-27 21:13:34 -0500

    added some stuff to server.js

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var express = require('express');
 var expHBars = require('express-handlebars');
 var methodOverride = require('method-override');
-var body-parser = require('body-parser');
+var bodyParser = require('body-parser');
+var burgerController = require('./controllers/burger_controller.js')
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.engine("handlebars", expHBars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
