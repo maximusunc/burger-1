e5a76e75809d9da8f96e17da0280abaebaf64a11
Taliesin <tdcochran2@gmail.com>
2017-11-27 21:22:51 -0500

    added logic to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
 var mysql = require('mysql');
-module.exports = funciton() {
-	var connection = mysql.createConnection({
-		host: "localhost",
-		port:3306,
-		user:'root',
-		password: 'Yourjoyisyoursorrow6392!',
-		database: 'burgers_db'
-	});
-	connection.connect(function(err) {
-	  if (err) throw err;
-	  console.log("connected as id " + connection.threadId);
-	  connection.end();
-	});
-}
+var connection = mysql.createConnection({
+	host: "localhost",
+	port:3306,
+	user:'root',
+	password: 'Yourjoyisyoursorrow6392!',
+	database: 'burgers_db'
+});
+
+
+connection.connect(function(err) {
+	if(err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+})
+module.exports = connection;
