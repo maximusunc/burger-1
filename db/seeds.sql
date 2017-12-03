ca1c00a974a8cb29a44e2322ca7c9e9d27f491ff
Taliesin <tdcochran2@gmail.com>
2017-12-03 12:48:56 -0500

    about to push to heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,4 +3,5 @@ INSERT INTO burgers (burger_name) VALUES ("The Sound and the Curry Burger");
 INSERT INTO burgers (burger_name) VALUES  ("New Baconings Burger");
 INSERT INTO burgers (burger_name) VALUES ("These Collards Don't Run Burger");
 INSERT INTO burgers (burger_name) VALUES ("Don't You Four Cheddar 'Bout Me Burger");
-INSERT INTO burgers (burger_name) VALUES ("Baby You Can Chive My Car Burger")
+INSERT INTO burgers (burger_name) VALUES ("Baby You Can Chive My Car Burger");
+INSERT INTO burgers (burger_name) VALUES ("Babe the Big Blue Cheese Burger");
